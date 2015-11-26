c4c968b2c753a88cb5413118a776fe0701ab54ca
Logmytech <Logmytech@users.noreply.github.com>
2015-11-26 16:25:17 +0530

    Update english_to_entean.py

--- english_to_entean.py
+++ english_to_entean.py
@@ -1,9 +1,20 @@
+import sys
 
-FULL_ENGLISH_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-FULL_ENTEAN_ALPHABET =  "AZYXEWVTISRLPNOMQKJHUGFDCBazyxewvtisrlpnomqkjhugfdcb"
+PY2 = sys.version_info[0] == 2 #Returns True if Python version is 2 
 
+if PY2:
+    from string import maketrans
+else:
+    maketrans = str.maketrans
 
-tran_tab = str.maketrans(FULL_ENGLISH_ALPHABET, FULL_ENTEAN_ALPHABET)
+FULL_ENGLISH_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+FULL_ENTEAN_ALPHABET =  "AZYXEWVTISRLPNOMQKJHUGFDCB"
+
+FULL_ENGLISH_ALPHABET += FULL_ENGLISH_ALPHABET.lower()
+FULL_ENTEAN_ALPHABET += FULL_ENTEAN_ALPHABET.lower()
+
+tran_tab = maketrans(FULL_ENGLISH_ALPHABET, FULL_ENTEAN_ALPHABET)
+
 
 def to_entean(text):
     return(text.translate(tran_tab))
@@ -12,7 +23,10 @@ def to_entean(text):
 def main(text=None, with_output=True):
 
     if text is None:
-        text = input("Enter English text: ")
+        if PY2:
+            text = raw_input("Enter English text: ")
+        else:
+            text = input("Enter English text: ")
 
     if with_output:    
         print(text)
